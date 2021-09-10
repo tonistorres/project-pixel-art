02eaf529d15c6a5779a911e86df7fd4dcc88e040
tonistorres <sisvenda2011@gmail.com>
2021-09-10 15:58:08 -0300

    QT 02 Respondida e Avaliada pelo npm test Linter

---
+++ script.js
@@ -0,0 +1,23 @@
+//************************************************************************************ */
+//Direitos Intelectuais: https://projects.raspberrypi.org/pt-PT/projects/pixel-art/6
+//
+//Implementações: Tonis Torres 
+//*********************************************************************************** */
+window.onload = function () {
+painterElementsCollorPalette();
+}
+
+function painterElementsCollorPalette() {
+    let captureElement = document.querySelectorAll('.color');
+    for (let i = 0; i < captureElement.length; i += 1) {
+        if(i===0){
+            captureElement[i].style.background="red";
+        }else if(i===1){
+            captureElement[i].style.background="blue";
+        }else if(i===2){
+            captureElement[i].style.background="yellow";
+        }else{
+            captureElement[i].style.background="green";
+        }
+    }
+}
